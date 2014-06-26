e8674ac4195046f3671982083583b788282b11b2
Jon Nappi <moogar0880@gmail.com>
2014-06-26 15:53:49 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Dyn'
-copyright = u'2014, Jonathan Nappi, Cole Tuininga'
+copyright = u'2014, Dyn Inc, Jonathan Nappi, Cole Tuininga'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

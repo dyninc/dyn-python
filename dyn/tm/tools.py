334989d7cc8cd01708724371adfb29599671ac23
Jon Nappi <jnappi@dyn.com>
2014-09-28 18:18:09 -0400

    Added map_ips function, cleaned up some logic in change_ip

--- dyn/tm/tools.py
+++ dyn/tm/tools.py
@@ -4,6 +4,7 @@ common or complicated tasks one will likely find themselves needing to
 accomplish via the DynECT API
 """
 from ..compat import string_types
+
 __author__ = 'jnappi'
 
 
@@ -17,13 +18,11 @@ def change_ip(zone, from_ip, to, v6=False):
     :param to: Either a list of ip addresses or a single ip address that will
         overwrite from_ip
     :param v6: Boolean flag to specify if we're replacing ipv4 or ipv6 addresses
-        (ie, whether we're updating an ARecord or AAAARecord
+        (ie, whether we're updating an ARecord or AAAARecord)
     """
     publish = False
-    if v6:
-        records = zone.get_all_records_by_type('AAAA')
-    else:
-        records = zone.get_all_records_by_type('A')
+    records = zone.get_all_records_by_type('AAAA') if v6 else \
+        zone.get_all_records_by_type('A')
 
     def update_single_ip(f, t):
         changed = False
@@ -40,5 +39,30 @@ def change_ip(zone, from_ip, to, v6=False):
         if update_single_ip(ip, to[index]):
             publish = True
 
+    # If we made changes, publish the zone
+    if publish:
+        zone.publish()
+
+
+def map_ips(zone, mapping, v6=False):
+    """Change all occurances of an ip address to a new ip address under the
+    specified zone
+
+    :param zone: The :class:`~dyn.tm.zones.Zone` you wish to update ips for
+    :param mapping: A *dict* of the form {'old_ip': 'new_ip'}
+    :param v6: Boolean flag to specify if we're replacing ipv4 or ipv6 addresses
+        (ie, whether we're updating an ARecord or AAAARecord)
+    """
+    publish = False
+    records = zone.get_all_records_by_type('AAAA') if v6 else \
+        zone.get_all_records_by_type('A')
+
+    for old, new in mapping.items():
+        for record in records:
+            if record.address == old:
+                record.address = new
+                publish = True
+
+    # If we made changes, publish the zone
     if publish:
         zone.publish()

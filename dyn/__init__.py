ee90d58d65b004066fecf3630510a4d1df332f89
mhowes <mhowes@dyn.com>
2015-06-22 13:54:43 -0400

    Version Bump and Name Change.

--- dyn/__init__.py
+++ dyn/__init__.py
@@ -5,12 +5,12 @@ services.
 
 Requires Python 2.6 or higher, or the "simplejson" package.
 """
-version_info = (1, 3, 13)
+version_info = (1, 3, 14)
 __name__       = 'dyn'
 __doc__        = 'A python wrapper for the DynDNS and DynEmail APIs'
 __author__     = 'Jonathan Nappi, Cole Tuininga'
 __version__    = '.'.join([str(x) for x in version_info])
-__maintainer__ = 'Jonathan Nappi'
-__email__      = 'jnappi@dyn.com'
+__maintainer__ = 'Marc Howes'
+__email__      = 'mhowes@dyn.com'
 __status__     = 'Stable'
 __title__      = '{0} version {1}'.format(__name__, __version__)

2bb56c3dc9ac282c608b00ff41fb383eaa74a685
mhowes <mhowes@dyn.com>
2015-06-26 12:02:09 -0400

    Large version bump due to big changes in DSF.

--- dyn/__init__.py
+++ dyn/__init__.py
@@ -5,7 +5,7 @@ services.
 
 Requires Python 2.6 or higher, or the "simplejson" package.
 """
-version_info = (1, 3, 14)
+version_info = (1, 4, 0)
 __name__       = 'dyn'
 __doc__        = 'A python wrapper for the DynDNS and DynEmail APIs'
 __author__     = 'Jonathan Nappi, Cole Tuininga'

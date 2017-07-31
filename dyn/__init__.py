31bea13f254f875fc51cfbb4c02f0e7a9d7d9fef
rnorthover <rnorthover@dyn.com>
2017-07-31 14:39:43 -0400

    Version bump to 1.8.0

--- dyn/__init__.py
+++ dyn/__init__.py
@@ -5,12 +5,17 @@ services.
 
 Requires Python 2.6 or higher, or the "simplejson" package.
 """
-version_info = (1, 7, 10)
+version_info = (1, 8, 0)
 __name__ = 'dyn'
 __doc__ = 'A python wrapper for the DynDNS and DynEmail APIs'
-__author__ = 'Jonathan Nappi, Cole Tuininga, Marc Howes, Philip Andrews'
+__author__ = '''
+    Jonathan Nappi,
+    Cole Tuininga,
+    Marc Howes,
+    Philip Andrews,
+    Robert Northover'''
 __version__ = '.'.join([str(x) for x in version_info])
-__maintainer__ = 'Marc Howes'
-__email__ = 'mhowes@dyn.com'
+__maintainer__ = 'Robert Northover'
+__email__ = 'rnorthover@dyn.com'
 __status__ = 'Stable'
 __title__ = '{0} version {1}'.format(__name__, __version__)
